6a1d948d8a7ae6b5e40e4741a8c4297966568d29
Sudeis <sudeisfed@gmail.com>
2026-06-24 23:37:44 +0300

    fix: add django_celery_beat to INSTALLED_APPS

--- server/config/settings.py
+++ server/config/settings.py
@@ -69,6 +69,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django_celery_results',
+    'django_celery_beat',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',     
